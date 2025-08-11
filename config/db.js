03cb7421665491560a268f8582764124d7e76752
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2025-08-11 15:27:35 +0530

    re-arrange stuff and export client

--- config/db.js
+++ config/db.js
@@ -1,13 +1,13 @@
 import { MongoClient } from "mongodb";
 
 const uri = process.env.DATABASE_URI;
-if (!uri) {
-    throw new Error("Connection String Error");
+if (typeof uri !== "string" || uri.length === 0) {
+    throw new Error("Invalid connection string in DATABASE_URI env var");
 }
 
-const client = new MongoClient(uri);
+export const client = new MongoClient(uri);
 
-export async function run() {
+export async function connectDatabase() {
     try {
         await client.connect();
         console.log("MongoDB connected");
